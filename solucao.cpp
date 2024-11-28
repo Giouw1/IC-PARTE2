7937296405a1b37bf7a2a290a6f93749593981fc
Giovanni Faletti Almeida <130237551+Giouw1@users.noreply.github.com>
2024-11-28 10:43:25 -0300

    Update solucao.cpp

--- solucao.cpp
+++ solucao.cpp
@@ -2,14 +2,7 @@
 #include "gera_grafos.hpp"
 using namespace std;
 
-    //Boa opção é definir o primeiro f e o primeiro hB antes de começar a execução: resolver agora isso
 
-    //Dúvida no bestsol inicial
-    //h são os felizes, hB são não felizes, f são os felizíveis: preciso do hB
-    //Grau nesse código acaba sendo algo como o custo de pintar: preciso garantir o bom funcionamento da revisão dos graus: se isso funcionar, fechou. se não, pensar na redefinição do hb e f
-    //O que fazer: acertar o redefinir graus(grausl), redefinir o hB e f para o que não contém v: Solução para o redefinir parece simples, mas custo alto. Preciso redefinir o hB e f? Nesses casos, o que acontece não é que o chamado mesmo com o f e hB errado são redefinidos, e o candidato é escolhido corretamente? talvez tenha uma chamada a mais, mas e daí?
-    //Acho que dá certo pois o que contém v procura os novos candidatos corretamente, assim como o que não contém v: minha dúvida e se a análise das condições vai fazer sentido (acho que não). pensar anas analsies de condições, gastar uns 15 minutos focando entendendo o código
-    //Erro de lógica: não faz sentido os dois f's serem iguais, os coloridol dado que v é feliz e dado q v não é feliz são diferentes: escolher candidatos, redefinir graus, redefinir hB e f para o não contendo: terá um hBL e um fL (um para v feliz e outro para v não feliz)
 int BT (grafo g, vector<int>graus, int k, unordered_set<int> h, unordered_set<int> hB, unordered_set<int> f, vector<bool> colorido = {},int bestsol = 0){
     int v = -1;
     int graumin = g.size()+1;
@@ -18,7 +11,7 @@ int BT (grafo g, vector<int>graus, int k, unordered_set<int> h, unordered_set<in
     //Todas as estruturas a seguir são implementadas para armazenar os conjuntos reavaliados após colorir o candidato
     unordered_set<int> hl= h;
     vector<int> grausl = graus;
-    vector<bool> coloridol = colorido; //Cópia dos coloridol pelo passo anterior, para poder tanto passar o parâmetro de um v colorido quanto de um v não colorido
+    vector<bool> coloridol = colorido;
     unordered_set<int> fl = f;
     unordered_set<int> hBl = hB;
     mt19937 gen(random_device{}());
@@ -57,10 +50,10 @@ int BT (grafo g, vector<int>graus, int k, unordered_set<int> h, unordered_set<in
 
     //Tornando V feliz verificando os ao redor e ajustando os seus graus: essencial, pois grau é o custo de deixar o vértice feliz dado K
     
-    for (int i = 0; i< g[v].size();i++){  //para cada vizinho de v e para cada vizinho dos vizinhos de v, reduzir o grau do vizinho em 1
+    for (int i = 0; i< g[v].size();i++){  //para cada vizinho de v e para cada vizinho dos vizinhos de v, reduzir o grau do vizinho em 1, caso nao tenham sido coloridos
         if (g[v][i] == 1){ //Se v já tinha sido colorido na etapa anterior, não precisa reavaliar vizinhos de v
             if (colorido[v] != 1){
-                grausl[i]--; //tira 1 grau pois o vértice v ficou feliz (caso o vértice v já não fosse colorido)
+                grausl[i]--; //tira 1 grau pois o vértice v ficou feliz, logo, colorido
             }
             if (colorido[i] != 1){ //se o vértice i já não tiver sido colorido por uma outra etapa
                 coloridol[i] = 1; //colore ele, e colore os vizinhos se eles ja nao forem coloridos
@@ -108,7 +101,8 @@ int BT (grafo g, vector<int>graus, int k, unordered_set<int> h, unordered_set<in
         
         if ((h.size()+f.size())>(bestsol)){
             bestsol = max(bestsol,(BT(g,graus,k,h,hB,f,colorido,bestsol))); 
-        }//não precisa dos dois casos, não tornei ninguém feliz para essa etapa
+        }//não tornei ninguém feliz para essa etapa: considera os mesmos conjuntos, retirando v de f e introduzindo em hB
+        //hB não é necessário no geral
 
     return bestsol;
 }
@@ -127,24 +121,13 @@ int felicidade_maxima(grafo g,vector<int> graus, int k){
     }
     return BT(g,graus,k,h,hB,coloriveis,coloridos,0);
 }
-//A lógica do algoritmo é a cada "etapa" tornar um vértice feliz: pagar o custo disso
-//Assumir que h é conjunto dos vértices coloridol
-//Primeira linha: interpretação: v é ( o vértice associado a) o argumento mínimo entre os vértices não coloridol
-//Levando em consideração que a soma dos graus de i é menor ou igual à quantidade de vértices à serem coloridol - a quantidade de vértices já coloridol
-//Na 1 etapa se colore v e os seus vizinhos.
 
-//Segunda linha: h/: vértices não coloríveis: são os vértices que dado K e a configuração atual já não são coloríveis
 
-//Terceira linha: f: vértices coloríveis: todos os vértices que dado K e a configuração atual são coloríveis
-
-// A lógica é tentar a possibilidade mais lógica: se h'+ f for maior que h', der para colorir: colore com essa possibilidade
-//Se h+f for maior do que h', tenta também: tenta as duas possibilidades: Caso h' for a melhor possibilidade, vai parar
-
-int main(){
+int main(){ //Isso aqui é o problema da mochila, com os custos variando: tentar fazer um bottom up pra melhorar
     grafo g;
     vector<int> graus; 
     tie(g,graus)= criador_grafos(6,9);
-    int b = felicidade_maxima(g,graus,2);
+    int b = felicidade_maxima(g,graus,5);
     cout << b<< endl;
     return 0;
 }
