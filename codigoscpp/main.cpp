3ff3486268d78c2487f0bd3a1e9f2b7a9a95be24
Giovanni Faletti Almeida <130237551+Giouw1@users.noreply.github.com>
2025-02-18 11:51:12 -0300

    Add files via upload

--- codigoscpp/main.cpp
+++ codigoscpp/main.cpp
@@ -7,24 +7,29 @@ using namespace std;
 #include "solucao.hpp"
 #include "gera_grafos.hpp"
 
-int main(int argc, char* argv[]) {
-    if (argc < 4) {  // Expecting at least 4 arguments (program name + 3 inputs)
+int main(/*int argc, char* argv[]*/) {
+    /*if (argc < 4) {  // Expecting at least 4 arguments (program name + 3 inputs)
         cerr << "Está faltando parâmetros: É um path, um inteiro(limite de tempo), e outro inteiro (seed)"<< endl;
         return 1;
     }
     string path = argv[1];     // First argument: file path
-    int limit = stoi(argv[2]); // Second argument: integer (converted from string)
-    int seed = stoi(argv[3]);
+    int k = stoi(argv[2]);
+    int limit = stoi(argv[3]); // Second argument: integer (converted from string)
+    int seed = stoi(argv[4]);*/
+    string path = "/home/aluno/Documents/giotemp/IC-PARTE2-main/pasta/graph_d_regular_k2_p0.3_v100_20241127_083958.txt";
+    int k = 50;
+    int limit = 60;
+    int seed = 0;
     ofstream arquivoresult("resultcpp.txt");
     grafo g;
     vector<int> graus;
-    int k;
+    int ka;
     int nodes;
     int solutionback;
     pair<grafo,vector<int>> get;
     unordered_set<int> happyback;
     unordered_set<int> coloredback;
-    tie(k,get) = leitor_grafos(path);
+    tie(ka,get) = leitor_grafos(path);
     g = get.first;
     graus = get.second;
     //lendo o grafo
@@ -33,10 +38,18 @@ int main(int argc, char* argv[]) {
     int tempo;
     pair<int,unordered_set<int>> getter;
     //executando backtracking
+    time_t timestamp = time(&timestamp);
+    struct tm datetime = *localtime(&timestamp);
+    cout << datetime.tm_min<< " "<< datetime.tm_sec<<endl;
     clock_start = clock();
     tie(nodes,getter) = felicidade_maxima(g,graus,k,clock_start,limit,seed);
-    clock_end = clock();
-    tempo = static_cast<int>(double((clock_end-clock_start)/CLOCKS_PER_SEC));
+    clock_end = clock();//checar essa questão do tempo
+    time_t timestampa = time(&timestampa);
+    struct tm datetimea = *localtime(&timestampa);
+    cout << datetimea.tm_min<< " "<< datetimea.tm_sec<<endl;
+    tempo = double((clock_end-clock_start)/CLOCKS_PER_SEC);
+    cout <<tempo<<endl;
+    cout<<nodes<<endl;
     solutionback = getter.first;
     happyback = getter.second;
     arquivoresult<< nodes << " "<< tempo << " "<< solutionback<<endl;
@@ -55,7 +68,7 @@ int main(int argc, char* argv[]) {
     vector<int> happyheur;
     int solutionheur;
     int ajuda;
-    for (int i = 0;i<25;i++){
+    for (int i = 0;i<25;i++){//marcar todos os resultados e ver os tempos--total e individual
         ajuda = solutionheur;
         solutionheur = felicidade_maxima(g,graus,k,seed);
         if (ajuda>solutionheur){
